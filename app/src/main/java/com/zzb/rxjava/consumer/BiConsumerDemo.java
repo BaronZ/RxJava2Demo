e9eb15b5d6932f47041ce0bfaa2e964dd5ec50ab
zzb <realbo89@outlook.com>
2017-12-25 11:19:46 +0800

    BiConsumer

--- app/src/main/java/com/zzb/rxjava/consumer/BiConsumerDemo.java
+++ app/src/main/java/com/zzb/rxjava/consumer/BiConsumerDemo.java
@@ -1,10 +1,14 @@
 package com.zzb.rxjava.consumer;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 
+import io.reactivex.Observable;
+import io.reactivex.Observer;
 import io.reactivex.Single;
 import io.reactivex.SingleEmitter;
 import io.reactivex.SingleOnSubscribe;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.BiConsumer;
 
 /**
@@ -29,5 +33,45 @@ public class BiConsumerDemo {
             }
         });
 
+    }
+
+    public void test2() {
+
+        Observer observer = Observable.just("").subscribeWith(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+    }
+    static abstract class IgnoreErrorConsumer<T> implements BiConsumer<T, Throwable> {
+        private static final String TAG = "IgnoreErrorConsumer";
+
+        @Override
+        public void accept(T t, Throwable throwable) throws Exception {
+            if (throwable == null) {
+                accept(t);
+            } else {
+                Log.e(TAG, "error", throwable);
+            }
+        }
+
+        protected abstract void accept(T t);
     }
 }
